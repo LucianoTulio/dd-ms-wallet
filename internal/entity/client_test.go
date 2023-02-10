40b52971fb0a493ad61aa2686cd5535808a14942
Luciano Tulio <luciano.tulio@platformbuilders.io>
2023-02-10 14:41:32 -0300

    update client method

--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -21,3 +21,18 @@ func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Nil(t, client)
 }
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("Joao Lasagna", "lasagna.bolonhesa@lasa.com")
+	err := client.Update("Joao Picanha", "picanha@pc.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "Joao Picanha", client.Name)
+	assert.Equal(t, "picanha@pc.com", client.Email)
+}
+
+func TestUpdateClientWithArgsAreInvalid(t *testing.T) {
+	client, _ := NewClient("Joao Lasagna", "lasagna.bolonhesa@lasa.com")
+	err := client.Update("", "picanha@pc.com")
+	assert.Error(t, err, "name is required")
+
+}
